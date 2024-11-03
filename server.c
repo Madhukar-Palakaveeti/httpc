8d31ff6034c90c3a44a16b042cb9527fdfe853bd
Madhukar-Palakaveeti <madhupalakaveeti@gmail.com>
2024-11-04 00:20:39 +0530

    First response!

--- server.c
+++ server.c
@@ -1,11 +1,12 @@
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <unistd.h>
 
 #define PORT 6969
 
