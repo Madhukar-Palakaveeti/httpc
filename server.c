029020f1bb316740f95bc12d3df6cd5dfbeb2728
Madhukar-Palakaveeti <madhupalakaveeti@gmail.com>
2024-11-05 21:52:32 +0530

    Added graceful exit

--- server.c
+++ server.c
@@ -55,40 +55,68 @@ int main() {
     exit(EXIT_FAILURE);
   }
 
-  printf("Listening on Port 6969!\n");
+  printf("Listening on Port %d!\n", PORT);
   cli_len = sizeof(cli_addr);
-  // for (;;) {
-  connfd = accept(sockfd, (struct sockaddr *)&cli_addr, &cli_len);
-  if (connfd < 0) {
-    printf("ERROR: Server not accepting!\n");
-    exit(EXIT_FAILURE);
+  for (;;) {
+    connfd = accept(sockfd, (struct sockaddr *)&cli_addr, &cli_len);
+    if (connfd < 0) {
+      printf("ERROR: Server not accepting!\n");
+      exit(EXIT_FAILURE);
+    }
+
+    printf("Successfully connected to the server\n");
+    char req[1024];
+    size_t req_len = sizeof(req) - 1;
+    ssize_t bytes_read = read(connfd, req, req_len);
+    req[bytes_read] = '\0';
+    // printf("%s\n", req);
+
+    RequestParams request_params = get_req_params(req, req_len);
+
+    // printf("HTTP VERB: %s\n", request_params.method);
+    // printf("Path: %s\n", request_params.path);
+    char *http_verb = request_params.method;
+    char *path = request_params.path;
+
+    char buff[1024];
+    if (strcmp(http_verb, "GET") == 0 && strcmp(path, "/quit") == 0) {
+      snprintf(buff, sizeof(buff),
+               "http/1.1 200 ok\r\n"
+               "server: http\r\n"
+               "content-type: text/html\r\n"
+               "connection: closed\r\n"
+               "\r\n"
+               "Connection closed from server side\n");
+      printf("Connection closed!\n");
+      write(connfd, buff, strlen(buff));
+      close(connfd);
+      break;
+    }
+
+    if (strcmp(http_verb, "GET") == 0) {
+      snprintf(buff, sizeof(buff),
+               "http/1.1 200 ok\r\n"
+               "server: http\r\n"
+               "content-type: text/html\r\n"
+               "connection: closed\r\n"
+               "\r\n"
+               "<h1>Hello Beautiful!</h1>\n");
+    }
+
+    else {
+      snprintf(buff, sizeof(buff),
+               "HTTP/1.1 201 Created\r\n"
+               "Server: Http\r\n"
+               "Content-Type: text/html\r\n"
+               "Connection: Closed\r\n"
+               "\r\n"
+               "{'status' : 'Item Created'}\n");
+    }
+
+    int bufflen = strlen(buff);
+    write(connfd, buff, bufflen);
+    close(connfd);
   }
-
-  printf("Successfully connected to the server\n");
-  char req[1024];
-  size_t req_len = sizeof(req) - 1;
-  ssize_t bytes_read = read(connfd, req, req_len);
-  req[bytes_read] = '\0';
-  // printf("%s\n", req);
-
-  RequestParams request_params = get_req_params(req, req_len);
-
-  printf("HTTP VERB: %s\n", request_params.method);
-  printf("Path: %s\n", request_params.path);
-
-  char buff[128];
-  snprintf(buff, sizeof(buff),
-           "HTTP/1.1 200 OK\r\n"
-           "Server: Http\r\n"
-           "Content-Type: text/html\r\n"
-           "Connection: Closed\r\n"
-           "\r\n"
-           "<h1>Hello Beautiful!</h1>\n");
-
-  int bufflen = strlen(buff);
-  write(connfd, buff, bufflen);
-  close(connfd);
-
   close(sockfd);
   return 0;
 }
