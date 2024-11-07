a46fa0e4f0269555e6d2ba38cbe62f13b716de78
Madhukar-Palakaveeti <madhupalakaveeti@gmail.com>
2024-11-08 03:20:32 +0530

    Added GET

--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+#include <linux/limits.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,6 +18,9 @@ typedef struct {
   char *name;
   double price;
 } Data;
+
+Data data[10];
+int row;
 
 typedef struct {
   char *first_line;
@@ -39,7 +43,7 @@ RequestParams get_req_params(char *req_buf, size_t req_len) {
   return request_params;
 }
 
-int insert_into_db(const char *req_body, Data *data) {
+int insert_into_db(const char *req_body) {
   cJSON *json = cJSON_Parse(req_body);
   if (json == NULL) {
     return -1;
@@ -49,12 +53,15 @@ int insert_into_db(const char *req_body, Data *data) {
   cJSON *name = cJSON_GetObjectItem(json, "name");
   cJSON *price = cJSON_GetObjectItem(json, "price");
 
-  data->id = id->valueint;
-  data->name = name->valuestring;
-  data->price = price->valuedouble;
+  int db_id = id->valueint;
+
+  data[db_id].id = id->valueint;
+  data[db_id].name = strdup(name->valuestring);
+
+  data[db_id].price = price->valuedouble;
 
   cJSON_Delete(json);
-  return (data->id);
+  return (db_id);
 }
 
 void build_http_response(char *res_buf, RequestParams *request_params,
@@ -64,13 +71,35 @@ void build_http_response(char *res_buf, RequestParams *request_params,
   char *body = request_params->body;
 
   if (strcmp(http_verb, "GET") == 0) {
-    snprintf(res_buf, 1024,
-             "HTTP/1.1 200 OK\r\n"
-             "server: Http\r\n"
-             "content-type: text/html\r\n"
-             "connection: closed\r\n"
-             "\r\n"
-             "<h1>Hello Beautiful!</h1>\n");
+    if (strcmp(path, "/") == 0) {
+      snprintf(res_buf, 1024,
+               "HTTP/1.1 200 OK\r\n"
+               "server: Http\r\n"
+               "content-type: text/html\r\n"
+               "connection: closed\r\n"
+               "\r\n"
+               "<h1>Hello Beautiful!</h1>\n");
+    } else {
+      int idpath = atoi(path + 1);
+      if (idpath < 0) {
+        snprintf(res_buf, 1024,
+                 "HTTP/1.1 404 OK\r\n"
+                 "server: Http\r\n"
+                 "content-type: text/html\r\n"
+                 "connection: closed\r\n"
+                 "\r\n"
+                 "{'Error' : 'Data Not Found'}");
+      } else {
+        snprintf(res_buf, 1024,
+                 "HTTP/1.1 200 OK\r\n"
+                 "server: Http\r\n"
+                 "content-type: text/html\r\n"
+                 "connection: closed\r\n"
+                 "\r\n"
+                 "{'id' : %d, 'name' : %s, 'price' : '%lf'}",
+                 data[idpath].id, data[idpath].name, data[idpath].price);
+      }
+    }
   }
 
   else {
@@ -88,7 +117,12 @@ void build_http_response(char *res_buf, RequestParams *request_params,
 int main() {
   int sockfd, connfd, cli_len;
   struct sockaddr_in my_addr, cli_addr;
-
+  // initializing the db
+  for (row = 0; row < 10; row++) {
+    data[row].id = 0;
+    data[row].name = NULL;
+    data[row].price = 0;
+  }
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
   if (sockfd < 0) {
@@ -130,12 +164,12 @@ int main() {
     ssize_t bytes_read = read(connfd, req, req_len);
     req[bytes_read] = '\0';
 
+    // printf("%s\n", req);
     RequestParams request_params = get_req_params(req, req_len);
     char *http_verb = request_params.method;
     char *path = request_params.path;
     const char *body = request_params.body;
 
-    Data data;
     int res_id = 0;
     char res[1024];
 
@@ -155,13 +189,19 @@ int main() {
     }
 
     if (strcmp(http_verb, "POST") == 0 && body[0] != '\0') {
-      res_id = insert_into_db(body, &data);
+      res_id = insert_into_db(body);
     }
 
     build_http_response(res, &request_params, res_id);
     int res_len = strlen(res);
     write(connfd, res, res_len);
+
     close(connfd);
+  }
+  for (int i = 0; i < 10; i++) {
+    if (data[i].name != NULL && data[i].name[0] != '\0') {
+      free(data[i].name);
+    }
   }
   close(sockfd);
   return 0;
